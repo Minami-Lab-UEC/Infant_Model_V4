19d87d352527994fc8c350c970b5c8661982cd8c
ricky-of-takesuta <t2131098@edu.cc.uec.ac.jp>
2022-12-09 02:06:07 +0900

    opencvのbuildとdensetrackのsetupをdockerfileに記載

--- docker_take/takeshita_train.sh
+++ docker_take/takeshita_train.sh
@@ -2,4 +2,4 @@ docker run --gpus all -d \
 	--volume /home/takeshita/Infant_Model_V4:/home/takeshita/Infant_Model_V4 \
 	-w /home/takeshita/Infant_Model_V4 \
 	takeshita \
-	python pre_main_V2_noun.py
+	python pre_main.py
