75005215fec925a1cf71786a605f4cf38536bc7e
ricky-of-takesuta <t2131098@edu.cc.uec.ac.jp>
2022-12-14 16:27:57 +0900

    既存の動作について動画を追加

--- densetrack_test_takeshita.py
+++ densetrack_test_takeshita.py
@@ -3,18 +3,23 @@ import numpy as np
 import skvideo.io
 import densetrack
 
+# eat * 6, throw * 3, read * 3, stack * 3
 video_file_name_list = [
-        'Moments_in_Time_Mini/training/crying/getty-asian-baby-cry-video-id805355932_4.mp4',
-        'Moments_in_Time_Mini/training/crying/flickr-5-3-2-9-5-1-4-2-6153295142_19.mp4',
-        # 'Moments_in_Time_Mini/training/crying/getty-sad-real-man-crying-video-id688113760_10.mp4',
-        'Moments_in_Time_Mini/training/crying/yt-tvvMDShjkc8_44.mp4',
-        'Moments_in_Time_Mini/training/walking/flickr-7-7-6-0-1-0-9-1-4877601091_1.mp4',
-        #'Moments_in_Time_Mini/training/walking/flickr-1-6-7-3-3-8-3-6-18716733836_40.mp4',
-        'Moments_in_Time_Mini/training/walking/yt-X4tzjjxeJVY_379.mp4',
-        'Moments_in_Time_Mini/training/walking/4-4-5-1-5-0-0-7-3244515007_30.mp4',
-        'Moments_in_Time_Mini/training/running/flickr-4-3-2-7-4-8-8-6-16943274886_9.mp4',
-        'Moments_in_Time_Mini/training/running/flickr-3-7-5-6078422375_7.mp4',
-        'Moments_in_Time_Mini/training/running/flickr-0-7-2-6-2-1-1-1-20107262111_3.mp4'
+        'Moments_in_Time_Mini/training/biting/getty-beautiful-blonde-eating-a-green-apple-video-id598747074_5.mp4',
+        'Moments_in_Time_Mini/training/biting/getty-baby-boy-fruit-video-id146171604_2.mp4',
+        'Moments_in_Time_Mini/training/biting/getty-baby-teething-problem-video-id692394240_1.mp4',
+        'Moments_in_Time_Mini/training/biting/getty-angry-cat-biting-and-hissing-video-id489683554_2.mp4',
+        'Moments_in_Time_Mini/training/biting/getty-babies-ruining-smartphones-video-id686040830_19.mp4',
+        'Moments_in_Time_Mini/training/biting/getty-babies-ruining-smartphones-video-id682718098_5.mp4',
+        'Moments_in_Time_Mini/training/playing+sports/getty-arizona-wranglers-quarterback-alan-risher-throwing-pass-to-teammate-video-id1377-34_2.mp4',
+        'Moments_in_Time_Mini/training/playing+sports/getty-behind-view-of-boy-playing-catch-video-id647330573_30.mp4',
+        'Moments_in_Time_Mini/training/playing+sports/getty-father-teaching-his-sons-how-to-play-american-football-slow-motion-video-id467201707_10.mp4',
+        'Moments_in_Time_Mini/training/studying/flickr-0-7-3-6-4-6-3-2-6307364632_59.mp4',
+        'Moments_in_Time_Mini/training/studying/getty-boy-doing-homework-american-fork-utah-usa-video-id93232127_4.mp4',
+        'Moments_in_Time_Mini/training/studying/2jyueyJmiEQ_119.mp4',
+        'Moments_in_Time_Mini/training/assembling/flickr-5-9-9-1-5-3-6-5-15559915365_32.mp4',
+        'Moments_in_Time_Mini/training/stacking/getty-arranging-orange-and-pepper-slices-video-id162682612_26.mp4',
+        'Moments_in_Time_Mini/training/stacking/getty-afar-men-build-stone-enclosure-on-august-16-2011-on-road-from-makele-video-id139111660_9.mp4',
 ]
 
 data_dir = 'test_densetrack'
