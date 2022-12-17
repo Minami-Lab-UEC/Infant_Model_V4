3bf01cf6ae33231635d13271cf1d2f5930a3a5ee
ricky-of-takesuta <t2131098@edu.cc.uec.ac.jp>
2022-12-17 13:02:39 +0900

    動き特徴量の差し替え

--- make_table-master/main_add_move_fv.py
+++ make_table-master/main_add_move_fv.py
@@ -1,11 +1,32 @@
 import pandas as pd
 
-main_df_1 = pd.read_csv('../table/main_new.csv')
+main_df = pd.read_csv('../table/main_new.csv')
 move_df = pd.read_csv('densetrack_create_takeshita/new_move_k_medoids_100_30.csv')
 
 # 動画特徴量を追加するためデータセットを2倍のサイズにする
-main_df_2 = main_df_1.copy()
-main_df = pd.concat([main_df_1, main_df_2])
+# main_df_2 = main_df_1.copy()
+# main_df = pd.concat([main_df_1, main_df_2])
 
-# idを削除してindexを振り直す
-main_df.reset_index(drop=True).drop('id', axis=1)
+# # idを削除してindexを振り直す
+# main_df = main_df.reset_index(drop=True).drop('id', axis=1)
+
+# # row[1]:id
+# # row[2:]:move feature value
+# for row in move_df.itertuples(name=None):
+# 	# -57600からmove feature valueのデータが保存されている
+# 	main_df.iloc[row[1]][-57600:] = row[2:]
+
+# 20221217 main_dfの動き特徴量をすべて入れ替える #
+# 既存の動き特徴量を削除
+main_df = main_df.drop(columns=main_df.columns[-57600:])
+
+# move_dfのidを1~14に置き換える
+move_df = move_df.drop('id', axis=1)
+move_df = move_df.reset_index()
+move_df = move_df.rename(columns={'index':'id'})
+
+# merge, id基準
+main_df = pd.merge(main_df, move_df, on='id')
+
+# 保存
+main_df.to_csv('densetrack_create_takeshita/main_new.csv', index=False)
