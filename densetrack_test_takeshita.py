a11806614bc407741bbaf09b674be924a08971e7
ricky-of-takesuta <t2131098@edu.cc.uec.ac.jp>
2022-12-19 02:18:09 +0900

    動画特徴量の追加

--- densetrack_test_takeshita.py
+++ densetrack_test_takeshita.py
@@ -4,41 +4,42 @@ import skvideo.io
 import densetrack
 
 # eat * 6, throw * 3, read * 3, stack * 3
-# video_file_name_list = [
-#         'Moments_in_Time_Mini/training/biting/getty-beautiful-blonde-eating-a-green-apple-video-id598747074_5.mp4',
-#         'Moments_in_Time_Mini/training/biting/getty-baby-boy-fruit-video-id146171604_2.mp4',
-#         'Moments_in_Time_Mini/training/biting/getty-baby-teething-problem-video-id692394240_1.mp4',
-#         'Moments_in_Time_Mini/training/biting/getty-angry-cat-biting-and-hissing-video-id489683554_2.mp4',
-#         'Moments_in_Time_Mini/training/biting/getty-babies-ruining-smartphones-video-id686040830_19.mp4',
-#         'Moments_in_Time_Mini/training/biting/getty-babies-ruining-smartphones-video-id682718098_5.mp4',
-#         'Moments_in_Time_Mini/training/playing+sports/getty-arizona-wranglers-quarterback-alan-risher-throwing-pass-to-teammate-video-id1377-34_2.mp4',
-#         'Moments_in_Time_Mini/training/playing+sports/getty-behind-view-of-boy-playing-catch-video-id647330573_30.mp4',
-#         'Moments_in_Time_Mini/training/playing+sports/getty-father-teaching-his-sons-how-to-play-american-football-slow-motion-video-id467201707_10.mp4',
-#         'Moments_in_Time_Mini/training/studying/flickr-0-7-3-6-4-6-3-2-6307364632_59.mp4',
-#         'Moments_in_Time_Mini/training/studying/getty-boy-doing-homework-american-fork-utah-usa-video-id93232127_4.mp4',
-#         'Moments_in_Time_Mini/training/studying/2jyueyJmiEQ_119.mp4',
-#         'Moments_in_Time_Mini/training/assembling/flickr-5-9-9-1-5-3-6-5-15559915365_32.mp4',
-#         'Moments_in_Time_Mini/training/stacking/getty-arranging-orange-and-pepper-slices-video-id162682612_26.mp4',
-#         'Moments_in_Time_Mini/training/stacking/getty-afar-men-build-stone-enclosure-on-august-16-2011-on-road-from-makele-video-id139111660_9.mp4',
-# ]
+video_file_name_list = [
+        'Moments_in_Time_Mini/training/biting/getty-baby-teething-problem-video-id692394240_1.mp4',
+	'Moments_in_Time_Mini/training/biting/getty-blond-haired-woman-eating-an-apple-in-her-kitchen-video-id73246632_5.mp4',
+	'Moments_in_Time_Mini/training/biting/getty-blond-haired-woman-laughing-and-smiling-whilst-eating-an-apple-video-id73246634_21.mp4',
+	'Moments_in_Time_Mini/training/biting/getty-boy-eats-a-hamburger-video-id101300656_2.mp4',
+	'Moments_in_Time_Mini/training/biting/getty-boy-eats-apple-video-id473102205_11.mp4',
+	'Moments_in_Time_Mini/training/biting/getty-brother-and-sister-eating-healthy-snacks-and-reading-book-at-kitchen-video-id1002-26_12.mp4',
+        'Moments_in_Time_Mini/training/playing+sports/getty-father-teaching-his-sons-how-to-play-american-football-slow-motion-video-id467201725_24.mp4',
+	'Moments_in_Time_Mini/training/playing+sports/getty-man-throwing-football-in-park-provo-utah-usa-video-id97374053_4.mp4',
+	'Moments_in_Time_Mini/training/playing+sports/giphy-JV93Bi5lbWnle_2.mp4',
+	'Moments_in_Time_Mini/training/studying/getty-boy-reading-book-on-floor-in-school-library-video-id454427828_17.mp4',
+	'Moments_in_Time_Mini/training/studying/getty-college-student-with-books-researching-in-library-video-id595456004_4.mp4',
+	'Moments_in_Time_Mini/training/studying/getty-children-studying-video-id156870300_1.mp4',
+	'Moments_in_Time_Mini/training/stacking/getty-workers-stacking-crates-of-tnt-atop-wooden-platform-for-the-trinity-video-idmr_00092377_18.mp4',
+	'Moments_in_Time_Mini/training/stacking/getty-workers-moving-stacks-of-50-dollar-bills-on-table-kansas-city-kansas-video-id125128656_10.mp4',
+        'Moments_in_Time_Mini/training/stacking/getty-little-girl-playing-with-blocks-video-id488280395_13.mp4'
+]
 
-DATA_DIR = 'track_length_90over'
+DATA_DIR = 'test_densetrack'
 MOVIE_DIR = 'Moments_in_Time_Mini/training'
-VERB_LIST = ['eating', 'biting', 'playing+sports', 'studying', 'assembling', 'stacking', 'throwing', 'reading/']
+# VERB_LIST = ['eating', 'biting', 'playing+sports', 'studying', 'assembling', 'stacking', 'throwing', 'reading/']
 # VERB_LIST = ['eating']
 
-for verb in VERB_LIST:
-        video_file_name_list = os.listdir(os.path.join(MOVIE_DIR, verb))
-        os.makedirs(os.path.join(DATA_DIR, verb), exist_ok=True)
-        for video_file_name in video_file_name_list:
-                video_frames = skvideo.io.vreader(fname=os.path.join(MOVIE_DIR, verb, video_file_name), as_grey=True)
-                video_gray = np.stack([np.reshape(x, x.shape[1:3])
-                                for x in video_frames]).astype(np.uint8, copy=False)
-                tracks = densetrack.densetrack(video_gray, track_length=15
-                , adjust_camera=True)
-                if len(tracks) == 0:
-                        continue
-                # print('track_length_90over! : ', video_file_name)
-                head, tail = os.path.split(video_file_name)
-                name = os.path.splitext(tail)[0]
-                np.save(os.path.join(DATA_DIR, verb, name + '-traj'), tracks)
+# for verb in VERB_LIST:
+# video_file_name_list = os.listdir(os.path.join(MOVIE_DIR, verb))
+# os.makedirs(os.path.join(DATA_DIR, verb), exist_ok=True)
+for video_file_name in video_file_name_list:
+        video_frames = skvideo.io.vreader(fname=video_file_name, as_grey=True)
+        video_gray = np.stack([np.reshape(x, x.shape[1:3])
+                        for x in video_frames]).astype(np.uint8, copy=False)
+        tracks = densetrack.densetrack(video_gray, track_length=15
+        , adjust_camera=True)
+        if len(tracks) == 0:
+                continue
+        # print('track_length_90over! : ', video_file_name)
+        head, tail = os.path.split(video_file_name)
+        name = os.path.splitext(tail)[0]
+        # np.save(os.path.join(DATA_DIR, verb, name + '-traj'), tracks)
+        np.save(os.path.join(DATA_DIR, name + '-traj'), tracks)
--- make_table-master/main_add_move_fv.py
+++ make_table-master/main_add_move_fv.py
@@ -1,32 +1,34 @@
 import pandas as pd
 
-main_df = pd.read_csv('../table/main_new.csv')
-move_df = pd.read_csv('densetrack_create_takeshita/new_move_k_medoids_100_30.csv')
+main_df_1 = pd.read_csv('densetrack_create_takeshita/main_new.csv')
+move_df = pd.read_csv('densetrack_create_takeshita/new_move_k_medoids_100_30_1217.csv')
 
 # 動画特徴量を追加するためデータセットを2倍のサイズにする
-# main_df_2 = main_df_1.copy()
-# main_df = pd.concat([main_df_1, main_df_2])
+main_df_2 = main_df_1.copy()
+main_df = pd.concat([main_df_1, main_df_2])
 
 # # idを削除してindexを振り直す
-# main_df = main_df.reset_index(drop=True).drop('id', axis=1)
+main_df = main_df.drop('id', axis=1).reset_index(drop=True)
 
 # # row[1]:id
 # # row[2:]:move feature value
-# for row in move_df.itertuples(name=None):
-# 	# -57600からmove feature valueのデータが保存されている
-# 	main_df.iloc[row[1]][-57600:] = row[2:]
+for row in move_df.itertuples(name=None):
+	# -42600からmove feature valueのデータが保存されている
+	main_df.iloc[row[1], -42600:] = row[2:]
 
+main_df = main_df.reset_index()
+main_df = main_df.rename(columns={'index':'id'})
 # 20221217 main_dfの動き特徴量をすべて入れ替える #
 # 既存の動き特徴量を削除
-main_df = main_df.drop(columns=main_df.columns[-57600:])
+# main_df = main_df.drop(columns=main_df.columns[-57600:])
 
-# move_dfのidを1~14に置き換える
-move_df = move_df.drop('id', axis=1)
-move_df = move_df.reset_index()
-move_df = move_df.rename(columns={'index':'id'})
+# # move_dfのidを1~14に置き換える
+# move_df = move_df.drop('id', axis=1)
+# move_df = move_df.reset_index()
+# move_df = move_df.rename(columns={'index':'id'})
 
-# merge, id基準
-main_df = pd.merge(main_df, move_df, on='id')
+# # merge, id基準
+# main_df = pd.merge(main_df, move_df, on='id')
 
-# 保存
-main_df.to_csv('densetrack_create_takeshita/main_new.csv', index=False)
+# # 保存
+main_df.to_csv('densetrack_create_takeshita/main_new_1219.csv', index=False)
